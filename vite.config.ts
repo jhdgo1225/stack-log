14bbe30718d4f4b13c3c5c9af5ee3c0cd24ccde7
jhdgo1225 <parkjh99365@gmail.com>
2026-06-02 15:37:25 +0900

    CHORE: add vanilla-extract in vite.config.ts for my Vite Project

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { fileURLToPath, URL } from "node:url";
 
 import react from "@vitejs/plugin-react";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [vanillaExtractPlugin(), react()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
