834edd6a601f92fbb6249d67b449c24d37b16622
jhdgo1225 <parkjh99365@gmail.com>
2026-06-06 01:55:04 +0900

    FEAT: Asset 반환 함수 export

--- src/entities/character/index.ts
+++ src/entities/character/index.ts
@@ -1,3 +1,12 @@
 export { CHARACTER_LIST } from "./model/characters";
+export {
+  getCharacterFaceSrc,
+  getCharacterBlockSrc,
+  getCharacterGamePlayThemeSrc,
+  getCharacterImageSrc,
+  getCharacterMainThemeSrc,
+  getCharacterSkillSrc,
+  getCharacterSymbolSrc,
+} from "./model/assets";
 export { useCharacterStore } from "./model/characterStore";
 export type { Character, CharacterSkill } from "./model/types";
